48301b6b515f93f0ef3471bb42e2cb3bf590d784
Garikoitz <garikoitz@gmail.com>
2019-07-10 01:30:23 -0400

    files copied from winawerlab that I previously modified to make PRFmodel work better

--- stats/fitnonlinearmodel_backup.m
+++ stats/fitnonlinearmodel_backup.m
@@ -49,6 +49,11 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %       I (optional) is a function that takes fitted parameters (1 x P) from the 
 %         previous model and outputs a function that transforms stimuli into a 
 %         new form prior to model evaluation.
+%    OR
+%     M where M is a function that takes stimuli (N x C) and responses (N x 1) and
+%       outputs an estimate of the linear weights (1 x C).  For example, simple
+%       OLS regression is the case where M is @(X,y) (inv(X'*X)*X'*y)'.
+%       This case is referred to as the linear-model case.
 %
 %   *** SEED ***
 %   <seed> (optional) is:
@@ -58,6 +63,7 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %     (3) a function that accepts a single voxel index and returns (1) or (2).
 %         in this case, <vxs> must be supplied.
 %     If supplied, <seed> overrides the contents of X in <model>.
+%     In the linear-model case, <seed> is not applicable and should be [].
 %
 %   *** OPTIMIZATION OPTIONS ***
 %   <optimoptions> (optional) are optimization options in the form used by optimset.m.
@@ -68,12 +74,14 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %                'TolFun',1e-6,'TolX',1e-6, ...
 %                'OutputFcn',@(a,b,c) outputfcnsanitycheck(a,b,c,1e-6,10))
 %     In particular, it may be useful to specify a specific optimization algorithm to use.
+%     In the linear-model case, <optimoptions> is ignored.
 %   <outputfcn> (optional) is a function suitable for use as an 'OutputFcn'.  If you
 %     supply <outputfcn>, it will take precedence over any 'OutputFcn' in <optimoptions>.
 %     The reason for <outputfcn> is that the data points being fitted will be passed as a
 %     fourth argument to <outputfcn> (if <outputfcn> accepts four arguments).  This 
 %     enables some useful functionality such as being able to visualize the model and
 %     the data during the optimization.
+%     In the linear-model case, <outputfcn> is ignored.
 %
 %   *** RESAMPLING SCHEMES ***
 %   <wantresampleruns> (optional) is whether to resample at the level of runs (as opposed
@@ -121,23 +129,29 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %   *** OUTPUT-RELATED ***
 %   <dontsave> (optional) is a string or a cell vector of strings indicating outputs
 %     to omit when returning.  For example, you may want to omit 'testdata', 'modelpred',
-%     'numiters', and 'resnorms' since they may use a lot of memory.  If [] or not
-%     supplied, then we use the default of {'numiters' 'resnorms'}.  If {}, then we 
-%     will return all outputs.
+%     'modelfit', 'numiters', and 'resnorms' since they may use a lot of memory.  
+%     If [] or not supplied, then we use the default of {'modelfit' 'numiters' 'resnorms'}.
+%     If {}, then we will return all outputs.  Note: <dontsave> can also refer to 
+%     auxiliary variables that are saved to the .mat files when <outputdir> is used.
+%   <dosave> (optional) is just like 'dontsave' except that the outputs specified here
+%     are guaranteed to be returned.  (<dosave> takes precedence over <dontsave>.)
+%     Default is {}.
 %
 % <chunksize> (optional) is the number of voxels to process in a single function call.
 %   The default is to process all voxels.
 % <chunknum> (optional) is the chunk number to process.  Default: 1.
 %
 % This function, fitnonlinearmodel.m, is essentially a wrapper around MATLAB's
-% lsqcurvefit.m function for the purposes of fitting nonlinear models to data.
+% lsqcurvefit.m function for the purposes of fitting nonlinear (and linear) models
+% to data.
 %
 % This function provides the following key benefits:
 % - Deals with input and output issues (making it easy to process many individual
 %   voxels and evaluate different models)
 % - Deals with resampling (cross-validation and bootstrapping)
-% - Makes it easy to evaluate multiple initial seeds (to avoid local minima)
-% - Makes it easy to perform stepwise fitting of models
+% - In the case of nonlinear models, makes it easy to evaluate multiple initial 
+%   seeds (to avoid local minima)
+% - In the case of nonlinear models, makes it easy to perform stepwise fitting of models
 %
 % Outputs:
 % - 'params' is resampling cases x parameters x voxels.
@@ -155,12 +169,21 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %     This is the aggregated testing data across the resampling schemes.
 % - 'modelpred' is time points x voxels.
 %     This is the aggregated model predictions across the resampling schemes.
+% - 'modelfit' is resampling cases x time points x voxels.
+%     This is the model fit for each resampling scheme.  Here, by "model fit"
+%     we mean the fit for each of the original stimuli based on the parameters
+%     estimated in a given resampling case; we do not mean the fit for each of the 
+%     stimuli involved in the fitting.  (For example, if there are 100 stimuli and 
+%     we are performing cross-validation, there will nevertheless be 100 time points
+%     in 'modelfit'.)  Also, note that 'modelfit' is the raw fit; it is not adjusted
+%     for <wantremovepoly> and <wantremoveextra>.
 % - 'numiters' is a cell vector of dimensions 1 x voxels.  Each element is
 %     is resampling cases x seeds x models.  These are the numbers of iterations
-%     used in the optimizations.
+%     used in the optimizations.  Note that 'numiters' is [] in the linear-model case.
 % - 'resnorms' is a cell vector of dimensions 1 x voxels.  Each element is
 %     is resampling cases x seeds.  These are the residual norms obtained
 %     in the optimizations.  This is useful for diagnosing multiple-seed issues.
+%     Note that 'resnorms' is [] in the linear-model case.
 %
 % Notes:
 % - Since we use %06d.mat to name output files, you should use no more than 999,999 chunks.
@@ -169,12 +192,22 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 %   by making it such that the prediction for a given data point is calculated as the
 %   average of the predicted responses for the individual stimulus frames associated with
 %   that data point.
-% - To control the scale of the computations, in the optimization call we divide the 
-%   data by its standard deviation and apply the exact same scaling to the model.  This
-%   has the effect of controlling the scale of the residuals.  This last-minute scaling 
-%   should have no effect on the final parameter estimates.
+% - In the case of nonlinear models, to control the scale of the computations, in the 
+%   optimization call we divide the data by its standard deviation and apply the exact 
+%   same scaling to the model.  This has the effect of controlling the scale of the 
+%   residuals.  This last-minute scaling should have no effect on the final parameter estimates.
 %
 % History:
+% - 2014/05/01 - change the main loop to parfor; some cosmetic tweaks;
+%                now, if no parameters are to be optimized, just return the initial seed
+% - 2013/10/02 - implement the linear-model case
+% - 2013/09/07 - fix bug (if polynomials or extra regressors were used in multiple runs,
+%                then they were not getting fit properly).
+% - 2013/09/07 - in fitnonlinearmodel_helper.m, convert to double in the call to lsqcurvefit;
+%                and perform a speed-up (don't compute modelfit if unwanted)
+% - 2013/09/04 - add totalnumvxs variable
+% - 2013/09/03 - allow <dontsave> to refer to the auxiliary variables
+% - 2013/09/02 - add 'modelfit' and adjust default for 'dontsave'; add 'dosave'
 % - 2013/08/28 - new outputs 'resnorms' and 'numiters'; last-minute data scaling; 
 %                tweak default handling of 'dontsave'
 % - 2013/08/18 - Initial version.
@@ -182,43 +215,23 @@ function results = fitnonlinearmodel(opt,chunksize,chunknum)
 % Example 1:
 % 
 % % first, a simple example
-%{
-        x = randn(100,1);
-        y = 2*x + 3 + randn(100,1);
-        % Create stimulus, data and model
-        stimulus = [x ones(100,1)];
-        data     = y;
-        % model has the data to feed the parameters in lsqcurvefit
-        % model = {{[1 1]    [-Inf -Inf; Inf Inf]     @(pp,dd) dd*pp'}};
-        % model = {{ x0    [lowerBound; upperBound]       fun}};
-        % x = lsqcurvefit(fun,x0,xdata,ydata,lb,ub)
-        model    = {{[1 1] [-Inf -Inf; Inf Inf] @(pp,dd) dd*pp'}};
-        opt = struct( ...
-                     'stimulus',stimulus, ...
-                     'data',data, ...
-                     'model',model);
-        results = fitnonlinearmodel(opt);
-        % Check the prediction
-        isequal(results.modelpred, (results.params*stimulus')')
-
-%} 
-% % now, try 100 bootstraps
-%{
-        opt.resampling = 100;
-        opt.optimoptions = {'Display' 'off'};  % turn off reporting
-        results = fitnonlinearmodel(opt);
-%}
-% % now, try leave-one-out cross-validation
-%{
-opt.resampling = -(2*(eye(100) - 0.5));
-results = fitnonlinearmodel(opt);
-%} 
+% x = randn(100,1);
+% y = 2*x + 3 + randn(100,1);
+% opt = struct( ...
+%   'stimulus',[x ones(100,1)], ...
+%   'data',y, ...
+%   'model',{{[1 1] [-Inf -Inf; Inf Inf] @(pp,dd) dd*pp'}});
+% results = fitnonlinearmodel(opt);
 % 
-%
-%
-%
-%
-%
+% % now, try 100 bootstraps
+% opt.resampling = 100;
+% opt.optimoptions = {'Display' 'off'};  % turn off reporting
+% results = fitnonlinearmodel(opt);
+% 
+% % now, try leave-one-out cross-validation
+% opt.resampling = -(2*(eye(100) - 0.5));
+% results = fitnonlinearmodel(opt);
+% 
 % Example 2:
 % 
 % % try a more complicated example.  we use 'outputfcn' to 
@@ -233,10 +246,21 @@ results = fitnonlinearmodel(opt);
 %             @(pp,dd) evalgaussian1d(pp,dd)}}, ...
 %   'outputfcn',@(a,b,c,d) pause2(.1) | outputfcnsanitycheck(a,b,c,1e-6,10) | outputfcnplot(a,b,c,1,d));
 % results = fitnonlinearmodel(opt);
+%
+% Example 3:
+%
+% % same as the first example in Example 1, but now we use the
+% % linear-model functionality
+% x = randn(100,1);
+% y = 2*x + 3 + randn(100,1);
+% opt = struct( ...
+%   'stimulus',[x ones(100,1)], ...
+%   'data',y, ...
+%   'model',@(X,y) (inv(X'*X)*X'*y)');
+% results = fitnonlinearmodel(opt);
 
 % internal notes:
 % - replaces fitprf.m, fitprfstatic.m, fitprfmulti.m, and fitprfstaticmulti.m
-% - not implemented: gradient descent, OLS
 % - some of the new features: opt struct format, fix projection matrix bug (must
 %   compute projection matrix based on concatenated regressors), multiple initial
 %   seeds are handled internally!, user must deal with model specifics like
@@ -247,6 +271,7 @@ results = fitnonlinearmodel(opt);
 %   myriad optimization options from the input level, drop run-separated metrics,
 %   drop the stimulus transformation speed-up (it was getting implemented in a 
 %   non-general way)
+% - regularization is its own thing? own code module?
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% REPORT
 
@@ -276,7 +301,8 @@ if isfield(opt,'vxs')
   else
     vxsfull = opt.vxs;
   end
-  vxsfull = sort(union([],flatten(opt.vxs)));
+  vxsfull = sort(union([],flatten(vxsfull)));
+  totalnumvxs = length(vxsfull);
 end
 
 % deal with chunksize and chunknum
@@ -298,7 +324,13 @@ if isa(opt.data,'function_handle')
   fprintf('*** fitnonlinearmodel: loading data. ***\n');
   if nargin(opt.data) == 0
     data = feval(opt.data);
-  else
+    if iscell(data)
+      totalnumvxs = size(data{1},2);
+    else
+      totalnumvxs = size(data,2);
+    end
+  else  % note that in this case, vxs should have been specified,
+        % so totalnumvxs should have already been calculated above.
     if isempty(chunksize)
       chunksize = length(vxsfull);
     end
@@ -307,6 +339,11 @@ if isa(opt.data,'function_handle')
   end
 else
   data = opt.data;
+  if iscell(data)
+    totalnumvxs = size(data{1},2);
+  else
+    totalnumvxs = size(data,2);
+  end
 end
 if ~iscell(data)
   data = {data};
@@ -314,13 +351,13 @@ end
 
 % deal with chunksize
 if isempty(chunksize)
-  chunksize = size(data{1},2);  % default is all voxels
+  chunksize = totalnumvxs;  % default is all voxels
 end
 
 % if not isvxscase, then we may still need to do chunking
 if ~isvxscase
-  vxs = chunking(1:size(data{1},2),chunksize,chunknum);
-  if ~isequal(vxs,1:size(data{1},2))
+  vxs = chunking(1:totalnumvxs,chunksize,chunknum);
+  if ~isequal(vxs,1:totalnumvxs)
     for p=1:length(data)
       data{p} = data{p}(:,vxs);
     end
@@ -335,15 +372,17 @@ fprintf(['*** fitnonlinearmodel: outputdir = %s, chunksize = %d, chunknum = %d\n
   opt.outputdir,chunksize,chunknum);
 
 % deal with model
-if ~iscell(opt.model{1})
+if ~isa(opt.model,'function_handle') && ~iscell(opt.model{1})
   opt.model = {opt.model};
 end
-for p=1:length(opt.model)
-  if length(opt.model{p}) < 4 || isempty(opt.model{p}{4})
-    if p==1
-      opt.model{p}{4} = @identity;
-    else
-      opt.model{p}{4} = @(ss) @identity;
+if ~isa(opt.model,'function_handle')
+  for p=1:length(opt.model)
+    if length(opt.model{p}) < 4 || isempty(opt.model{p}{4})
+      if p==1
+        opt.model{p}{4} = @identity;
+      else
+        opt.model{p}{4} = @(ss) @identity;
+      end
     end
   end
 end
@@ -429,10 +468,16 @@ if ~isfield(opt,'wantremoveextra') || isempty(opt.wantremoveextra)
   opt.wantremoveextra = 1;
 end
 if ~isfield(opt,'dontsave') || (isempty(opt.dontsave) && ~iscell(opt.dontsave))
-  opt.dontsave = {'numiters' 'resnorms'};
+  opt.dontsave = {'modelfit' 'numiters' 'resnorms'};
 end
 if ~iscell(opt.dontsave)
   opt.dontsave = {opt.dontsave};
+end
+if ~isfield(opt,'dosave') || isempty(opt.dosave)
+  opt.dosave = {};
+end
+if ~iscell(opt.dosave)
+  opt.dosave = {opt.dosave};
 end
 
 % make outputdir if necessary
@@ -452,7 +497,7 @@ numtime = cellfun(@(x) size(x,1),data);
 
 % save initial time
 if wantsave
-  saveexcept(outputfile,'data');
+  saveexcept(outputfile,[{'data'} setdiff(opt.dontsave,opt.dosave)]);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% LOAD SOME ITEMS
@@ -497,12 +542,22 @@ end
 
 % construct total regressors matrix for fitting purposes
 % (i.e. both polynomials and extra regressors)
+  % first, construct the run-wise regressors
 tmatrix = {};
 for p=1:length(data)
   tmatrix{p} = cat(2,polyregressors{p},extraregressors{p});
 end
+  % then, separate them using blkdiag
+temp = blkdiag(tmatrix{:});
+cnt = 0;
+for p=1:length(data)
+  tmatrix{p} = temp(cnt+(1:size(tmatrix{p},1)),:);
+  cnt = cnt + size(tmatrix{p},1);
+end
+clear temp;
 
 % construct special regressors matrix for the purposes of the <metric>
+  % first, construct the run-wise regressors
 smatrix = {};
 for p=1:length(data)
   temp = [];
@@ -514,16 +569,20 @@ for p=1:length(data)
   end
   smatrix{p} = temp;
 end
+  % then, separate them using blkdiag
+temp = blkdiag(smatrix{:});
+cnt = 0;
+for p=1:length(data)
+  smatrix{p} = temp(cnt+(1:size(smatrix{p},1)),:);
+  cnt = cnt + size(smatrix{p},1);
+end
+clear temp;
 
 % figure out trainfun and testfun for resampling
 switch resamplingmode
 case 'full'
   trainfun = {@(x) catcell(1,x)};
-  % If testfun is empty, then it does not give the modelpred values back.
-  % Even though it is not correct, let's make testfun the same as trainfun to
-  % have some values.
-  % testfun =  {@(x) []};
-  testfun = {@(x) catcell(1,x)};
+  testfun =  {@(x) []};
 case 'xval'
   trainfun = {};
   testfun =  {};
@@ -562,27 +621,29 @@ end
 
 % loop over voxels
 clear results0;
-for p=1:vnum
+parfor p=1:vnum
 
   % report
   fprintf('*** fitnonlinearmodel: processing voxel %d (%d of %d). ***\n',vxs(p),p,vnum);
   vtime = clock;  % start time for current voxel
 
   % get data and hack it in
-  opt.data = cellfun(@(x) x(:,p),data,'UniformOutput',0);
+  opt2 = opt;
+  opt2.data = cellfun(@(x) x(:,p),data,'UniformOutput',0);
 
   % get seed and hack it in
-  if ~isempty(opt.seed)
-    if isa(opt.seed,'function_handle')
-      seed0 = feval(opt.seed,vxs(p));
+  if ~isempty(opt2.seed)
+    assert(~isa(opt2.model,'function_handle'));  % sanity check
+    if isa(opt2.seed,'function_handle')
+      seed0 = feval(opt2.seed,vxs(p));
     else
-      seed0 = opt.seed;
+      seed0 = opt2.seed;
     end
-    opt.model{1}{1} = seed0;
+    opt2.model{1}{1} = seed0;
   end
   
   % call helper function to do the actual work
-  results0(p) = fitnonlinearmodel_helper(opt,stimulus,tmatrix,smatrix,trainfun,testfun);
+  results0(p) = fitnonlinearmodel_helper(opt2,stimulus,tmatrix,smatrix,trainfun,testfun);
 
   % report
   fprintf('*** fitnonlinearmodel: voxel %d (%d of %d) took %.1f seconds. ***\n', ...
@@ -595,6 +656,7 @@ results = struct;
 results.params = cat(3,results0.params);
 results.testdata = cat(2,results0.testdata);
 results.modelpred = cat(2,results0.modelpred);
+results.modelfit = cat(3,results0.modelfit);
 results.trainperformance = cat(1,results0.trainperformance).';
 results.testperformance  = cat(1,results0.testperformance).';
 results.aggregatedtestperformance = cat(2,results0.aggregatedtestperformance);
@@ -603,9 +665,17 @@ results.resnorms = cat(2,{results0.resnorms});
 
 % kill unwanted outputs
 for p=1:length(opt.dontsave)
+
+  % if member of dosave, keep it!
+  if ismember(opt.dontsave{p},opt.dosave)
+
+  % if not, then kill it (if it exists)!
+  else
     if isfield(results,opt.dontsave{p})
-        results = rmfield(results,opt.dontsave{p});
+      results = rmfield(results,opt.dontsave{p});
     end
+  end
+
 end
 
 % save results
